1eab37a35e981eb6d6081c8cb802db5850888ab1
Algirdas Stankunas <algirdas.stankunas@gmail.com>
2020-03-16 13:36:46 +0200

    student class

---
+++ LD/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LD
+{
+    class Student
+    {
+        public string Vardas { get; set; }
+
+        public string Pavarde { get; set; }
+
+        public List<double> Nd { get; set; }
+
+        public double Egz { get; set; }
+
+        public Student()
+
+        {
+
+            Nd = new List<double>();
+
+        }
+    }
+}
