c7e2158febf33c5adcfe53de47734853507812e9
Algirdas Stankūnas <47825660+AlgirdasDev@users.noreply.github.com>
2020-03-31 13:17:14 +0300

    Add files via upload

---
+++ documentation/html/search/all_2.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['student_2',['Student',['../class_l_d_1_1_student.html',1,'LD']]],
+  ['studentmanager_3',['StudentManager',['../class_l_d_1_1_student_manager.html',1,'LD']]]
+];
