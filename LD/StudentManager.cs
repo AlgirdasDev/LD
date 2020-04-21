2552a0ee9563a4fc30bfd14909c46321419ef776
Algirdas Stankūnas <47825660+AlgirdasDev@users.noreply.github.com>
2020-04-21 15:08:37 +0300

    Add files via upload

--- LD/StudentManager.cs
+++ LD/StudentManager.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -29,7 +30,8 @@ namespace LD
                     Console.WriteLine("Sukurti studenta pasirinkite (1)\n" +
                         "Perziureti studentu sarasa pasirinkti (2)\n" +
                         "Nuskaityti studentus is failo (3)\n" +
-                        "Studentu isvedimas i failus vargsiukai/kietiakai (4)\n" +
+                        "Studentu generavimas ir isvedimas i failus (4)\n" +
+                        "Studentu isveddimas i failus vargsiukai/kietiakai (5)\n" +
                         "Baigti programa iveskite (0)\n");
                     var input = Console.ReadLine();
                     if (int.Parse(input) == 1)
@@ -45,6 +47,10 @@ namespace LD
                         studentaifile(); 
                     }
                     if (int.Parse(input) == 4)
+                    {
+                        studentaigenerate();
+                    }
+                    if (int.Parse(input) == 5)
                     {
                         studentaiprint();
                     }
@@ -75,7 +81,7 @@ namespace LD
                 var generuoti = Console.ReadLine();
                 if (generuoti.ToUpper() == "Y")
                 {
-                    Random rnd= new Random(); ;
+                    Random rnd= new Random(); 
                     for (int i = 0; i < 3; i++)
                     {
                         student.Nd.Add(rnd.Next(1,11));
@@ -195,56 +201,142 @@ namespace LD
         }
         public void studentaifile() 
         {
-            List<String> path = new List<string>();
-            for(int i =1; i <= 3; i++) 
+            string path = @"C:\Users\algir\Desktop\Studentai.txt";
+            string[] records = File.ReadAllLines(path);
+            var eile = new List<String>();
+            var pirmaeil = true;
+            foreach (string record in records)
             {
-                string dir = @"C:\Users\algir\Desktop\Studentai"+i.ToString()+".txt";
-                path.Add(dir);
-            }
-            for (int j = 0; j < path.Count; j++)
-            {
-                Stopwatch sw = new Stopwatch();
-                sw.Start();
-                string[] records = File.ReadAllLines(path[j]);
-                var eile = new List<String>();
-                var pirmaeil = true;
-                foreach (string record in records)
+                if (pirmaeil)
                 {
-                    if (pirmaeil)
-                    {
-                        eile = record.Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList();
-                        pirmaeil = false;
-                        continue;
-                    }
-                    var student = new Student();
-                    var vardas = record.Split(' ', StringSplitOptions.RemoveEmptyEntries)?[0];
-                    var pavarde = record.Split(' ', StringSplitOptions.RemoveEmptyEntries)?[1];
-                    student.Vardas = vardas;
-                    student.Pavarde = pavarde;
-                    foreach (var studeile in eile.Where(nd => nd.Contains("ND")))
-                    {
-                        var pazymiai = record.Split(' ', StringSplitOptions.RemoveEmptyEntries)?[eile.IndexOf(studeile)];
-                        student.Nd.Add(double.Parse(pazymiai));
-                    }
-                    var EgzIndex = eile.IndexOf("Egz.");
-                    var egz = record.Split(' ', StringSplitOptions.RemoveEmptyEntries)?[EgzIndex];
-                    student.Egz = double.Parse(egz);
-                    Students.Add(student);
+                    eile = record.Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList();
+                    pirmaeil = false;
+                    continue;
                 }
-                galutinis();
-                sw.Stop();
-                Console.WriteLine("Time Taken-->{0} ms", sw.ElapsedMilliseconds);
-                
+                var student = new Student();
+                var vardas = record.Split(' ', StringSplitOptions.RemoveEmptyEntries)?[0];
+                var pavarde = record.Split(' ', StringSplitOptions.RemoveEmptyEntries)?[1];
+                student.Vardas = vardas;
+                student.Pavarde = pavarde;
+                foreach (var studeile in eile.Where(nd => nd.Contains("ND")))
+                {
+                    var pazymiai = record.Split(' ', StringSplitOptions.RemoveEmptyEntries)?[eile.IndexOf(studeile)];
+                    student.Nd.Add(double.Parse(pazymiai));
+                }
+                var EgzIndex = eile.IndexOf("Egz.");
+                var egz = record.Split(' ', StringSplitOptions.RemoveEmptyEntries)?[EgzIndex];
+                student.Egz = double.Parse(egz);
+                Students.Add(student);
+            }
+            galutinis();
+        }
+        public void studentaigenerate()
+        {
+            for (int i=1;i<=10000000; i++)
+            {
+                var student = new Student();
+                Random rnd = new Random();
+                var vardas = "Vardas" + i.ToString();
+                var pavarde = "Pavarde" + i.ToString();
+                var egz = rnd.Next(1, 11);
+                student.Vardas = vardas;
+                student.Pavarde = pavarde;
+                student.Egz = egz;
+                for (int j = 1; j <= 6; j++) 
+                {
+                    student.Nd.Add(rnd.Next(1, 11));
+                }
+                Students.Add(student);               
+            }
+            galutinis();
+            Stopwatch sw = new Stopwatch();
+
+            string strPath = Environment.GetFolderPath(
+                         System.Environment.SpecialFolder.DesktopDirectory);
+            string pathString = System.IO.Path.Combine(strPath, "Studentai");
+            System.IO.Directory.CreateDirectory(pathString);
+            for (int i = 1; i <= 5; i++) 
+            {
+                using (System.IO.StreamWriter file =
+                new System.IO.StreamWriter(pathString +@"\Studentai" + i.ToString() + ".txt"))
+                {
+                    
+                    if (i == 1)
+                    {
+                        sw.Start();
+                        file.WriteLine("{0, -20} {1, -20} {2, 20} {3,20} {4, 20} {5,20} {6, 20} {7,20} {8, 20}", "Vardas", "Pavarde", "ND1", "ND2", "ND3", "ND4", "ND5", "ND6", "Egz.\n");
+                        for (int j=0;j<1000;j++)
+                        { 
+                                file.WriteLine("{0, -20} {1, -20} {2, 20} {3,20} {4, 20} {5,20} {6, 20} {7,20} {8, 20}", Students[j].Vardas, Students[j].Pavarde, Students[j].Nd[0], Students[j].Nd[1], Students[j].Nd[2], Students[j].Nd[3], Students[j].Nd[4], Students[j].Nd[5], Students[j].Egz);
+                        }
+                        sw.Stop();
+                        Console.WriteLine("Time Taken-->{0} ms", sw.ElapsedMilliseconds);
+                    }
+                    if (i == 2)
+                    { 
+                        sw.Start();
+                        file.WriteLine("{0, -20} {1, -20} {2, 20} {3,20} {4, 20} {5,20} {6, 20} {7,20} {8, 20}", "Vardas", "Pavarde", "ND1", "ND2", "ND3", "ND4", "ND5", "ND6", "Egz.\n");
+                        for (int j = 0; j < 10000; j++)
+                        {
+                            file.WriteLine("{0, -20} {1, -20} {2, 20} {3,20} {4, 20} {5,20} {6, 20} {7,20} {8, 20}", Students[j].Vardas, Students[j].Pavarde, Students[j].Nd[0], Students[j].Nd[1], Students[j].Nd[2], Students[j].Nd[3], Students[j].Nd[4], Students[j].Nd[5], Students[j].Egz);
+                        }
+                        sw.Stop();
+                        Console.WriteLine("Time Taken-->{0} ms", sw.ElapsedMilliseconds);
+                    }
+                    if (i == 3)
+                    {
+                        sw.Start();
+                        file.WriteLine("{0, -20} {1, -20} {2, 20} {3,20} {4, 20} {5,20} {6, 20} {7,20} {8, 20}", "Vardas", "Pavarde", "ND1", "ND2", "ND3", "ND4", "ND5", "ND6", "Egz.\n");
+                        for (int j = 0; j < 100000; j++)
+                        {
+                            file.WriteLine("{0, -20} {1, -20} {2, 20} {3,20} {4, 20} {5,20} {6, 20} {7,20} {8, 20}", Students[j].Vardas, Students[j].Pavarde, Students[j].Nd[0], Students[j].Nd[1], Students[j].Nd[2], Students[j].Nd[3], Students[j].Nd[4], Students[j].Nd[5], Students[j].Egz);
+                        }
+                        sw.Stop();
+                        Console.WriteLine("Time Taken-->{0} ms", sw.ElapsedMilliseconds);
+                    }
+                    if (i == 4)
+                    {
+                        sw.Start();
+                        file.WriteLine("{0, -20} {1, -20} {2, 20} {3,20} {4, 20} {5,20} {6, 20} {7,20} {8, 20}", "Vardas", "Pavarde", "ND1", "ND2", "ND3", "ND4", "ND5", "ND6", "Egz.\n");
+                        for (int j = 0; j < 1000000; j++)
+                        {
+                            file.WriteLine("{0, -20} {1, -20} {2, 20} {3,20} {4, 20} {5,20} {6, 20} {7,20} {8, 20}", Students[j].Vardas, Students[j].Pavarde, Students[j].Nd[0], Students[j].Nd[1], Students[j].Nd[2], Students[j].Nd[3], Students[j].Nd[4], Students[j].Nd[5], Students[j].Egz);
+                        }
+                        sw.Stop();
+                        Console.WriteLine("Time Taken-->{0} ms", sw.ElapsedMilliseconds);
+                    }
+                    if (i == 5)
+                    {   
+                        sw.Start();
+                        file.WriteLine("{0, -20} {1, -20} {2, 20} {3,20} {4, 20} {5,20} {6, 20} {7,20} {8, 20}", "Vardas", "Pavarde", "ND1", "ND2", "ND3", "ND4", "ND5", "ND6", "Egz.\n");
+                        for (int j = 0; j < 10000000; j++)
+                        {
+                            file.WriteLine("{0, -20} {1, -20} {2, 20} {3,20} {4, 20} {5,20} {6, 20} {7,20} {8, 20}", Students[j].Vardas, Students[j].Pavarde, Students[j].Nd[0], Students[j].Nd[1], Students[j].Nd[2], Students[j].Nd[3], Students[j].Nd[4], Students[j].Nd[5], Students[j].Egz);
+                        }
+                        sw.Stop();
+                        Console.WriteLine("Time Taken-->{0} ms", sw.ElapsedMilliseconds);
+                    }
+                    
+                }
             }
         }
         public void studentaiprint() 
         {
-            string pathString = @"C:\Users\algir\Desktop\Studentai";
-            System.IO.Directory.CreateDirectory(pathString);
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            string strPath = Environment.GetFolderPath(
+                         System.Environment.SpecialFolder.DesktopDirectory);
+            string pathString1 = System.IO.Path.Combine(strPath, "Studentai");
+            string pathString2 = pathString1;
+            string vargsiukai = @"vargšiukai.txt";
+            string kietiakai = @"kietiakai.txt";
+            System.IO.Directory.CreateDirectory(pathString1);
+            pathString1 = System.IO.Path.Combine(pathString1, vargsiukai);
+            pathString2 = System.IO.Path.Combine(pathString2, kietiakai);
             using (System.IO.StreamWriter file1 =
-            new System.IO.StreamWriter(@"C:\Users\algir\Desktop\Studentai\vargšiukai.txt"))
+            new System.IO.StreamWriter(pathString1))
             using (System.IO.StreamWriter file2 =
-            new System.IO.StreamWriter(@"C:\Users\algir\Desktop\Studentai\kietiakai.txt"))
+            new System.IO.StreamWriter(pathString2))
                 {
                     file1.WriteLine("{0,-20} {1,-20} {2,20} {3,20}", "Vardas", "Pavarde", "Galutinis(Vid.)", "Galutinis(Med.)\n");
                     file2.WriteLine("{0,-20} {1,-20} {2,20} {3,20}", "Vardas", "Pavarde", "Galutinis(Vid.)", "Galutinis(Med.)\n");
@@ -254,8 +346,10 @@ namespace LD
                         file1.WriteLine("{0, -20} {1, -20} {2, 20} {3,20} ", student.Vardas, student.Pavarde, student.Galutinis.ToString("F"), student.Mediana.ToString("F"));
                     else
                         file2.WriteLine("{0, -20} {1, -20} {2, 20} {3,20} ", student.Vardas, student.Pavarde, student.Galutinis.ToString("F"), student.Mediana.ToString("F"));
+                    }
                 }
-                }
+            sw.Stop();
+            Console.WriteLine("Time Taken-->{0} ms", sw.ElapsedMilliseconds);
         }
     }
 }
